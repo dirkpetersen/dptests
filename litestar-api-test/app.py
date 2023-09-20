7c382d8ad6c670ed5256bd44af25f1ebb76cb6bf
Dirk Petersen <peterdir@ohsu.edu>
2023-09-19 22:02:30 -0700

    examples for python GUIs

--- litestar-api-test/app.py
+++ litestar-api-test/app.py
@@ -1,5 +1,13 @@
 from litestar import Litestar, get
 
+# look at these 
+# https://www.infoworld.com/article/3700689/3-python-web-frameworks-for-beautiful-front-ends.html
+# https://github.com/litestar-org/litestar-fullstack
+# https://github.com/v3ss0n/awesome-starlite/
+
+# https://anvil.works/
+# or Reflex.dev, NiceGUI 
+
 @get("/")
 async def hello_world() -> str:
     return "Hello, world!"
