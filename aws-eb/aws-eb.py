0d8fb181f4c9cd747a80e4e4003401d7c3f48fb8
Dirk Petersen <peterdir@ohsu.edu>
2024-01-15 14:51:14 -0800

    no unmount if keeprunning

--- aws-eb/aws-eb.py
+++ aws-eb/aws-eb.py
@@ -293,7 +293,8 @@ def subcmd_launch(args,cfg,bld,aws):
     rpid = rclone.mount(f':s3:{cfg.archivepath}/sources', f'{bld.eb_root}/sources_s3')
     print(f'rclone mount pid: {rpid}')
     bld.build_all_eb(ecfgroot, s3_prefix, include=args.include, exclude=args.exclude)
-    rclone.unmount(f'{bld.eb_root}/sources_s3')
+    if not args.keeprunning:
+        rclone.unmount(f'{bld.eb_root}/sources_s3')
     
 def subcmd_download(args,cfg,bld,aws):
 
@@ -778,7 +779,8 @@ class Builder:
             self.rclone_upload_compare = '--checksum'
             self.upload(self.eb_root, f':s3:{self.cfg.archivepath}', s3_prefix)
             if self.cfg.is_systemd_service_running('redis6') or self.cfg.is_systemd_service_running('redis'):
-                ret=subprocess.run(f'sudo juicefs umount --flush /mnt/share', shell=True, text=True)
+                if not self.args.keeprunning:
+                    ret=subprocess.run(f'sudo juicefs umount --flush /mnt/share', shell=True, text=True)
             msg = f'BUILD FINISHED. Tried {ebcnt} viable easyconfigs ({ebskipped} skipped), {bldcnt} packages built, {errcnt} builds failed.'
             if errpkg:
                 msg += f'\nFailed easyconfigs: {", ".join(errpkg)}'
