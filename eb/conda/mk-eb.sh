20c0e7f8cfdd83b7c86edf5726cc4e70541ece3e
Dirk Petersen <peterdir@ohsu.edu>
2024-01-18 10:45:34 -0800

    added working packges

--- eb/conda/mk-eb.sh
+++ eb/conda/mk-eb.sh
@@ -6,6 +6,5 @@
 while IFS= read -r line
 do
     /usr/bin/python3 eb_conda_configs.py module --packages bioconda/"$line"
-done < bioconda-packages.txt
-
+done < bioconda-packages-working-x86-64.txt
 
