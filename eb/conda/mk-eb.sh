428d90c3c703045183781685a804fd52557640f7
Dirk Petersen <peterdir@ohsu.edu>
2024-01-18 09:09:54 -0800

    eb batch

---
+++ eb/conda/mk-eb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+/usr/bin/python3 -m pip install anaconda-client jinja2
+
+while IFS= read -r line
+do
+    /usr/bin/python3 eb_conda_configs/eb_conda_configs.py module --packages bioconda/"$line"
+done < bioconda-packages.txt
+
+
--- eb/mk-eb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while IFS= read -r line
-do
-    python3 eb_conda_configs/eb_conda_configs.py module --packages bioconda/"$line"
-done < bioconda-packages.txt
-
-
