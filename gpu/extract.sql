1ab18a442f56209a76b1fbae6d67458ff25398f0
Dirk Petersen <no-reply@nowhere.com>
2025-03-26 09:14:23 -0700

    feat: Add GPU extract SQL script

---
+++ gpu/extract.sql
@@ -0,0 +1,20 @@
+COPY (
+    SELECT
+        h.hostname || '-' || p.username || '-' || p.command || '-' || p.pid::VARCHAR AS job,
+        g.uuid AS gpuid,
+        h.hostname AS hostname,
+        g.name AS gputype,
+        CAST(g.utilization->>'gpu' AS INTEGER) AS gpu_util,
+        CAST(g.memory->>'used' AS INTEGER) AS gpu_mem_total_used,
+        p.username,
+        p.command,
+        CAST(p.pid AS INTEGER) AS pid,
+        CAST(p.memory->>'gpu_memory_usage' AS INTEGER) AS gpu_memory_usage
+    FROM 'gpu_stats_merged.parquet' AS original
+    JOIN LATERAL json_transform(original.json_data) AS h
+    JOIN LATERAL json_transform(json(h.gpus)) AS g
+        ON g.index < json_array_length(h.gpus)
+    JOIN LATERAL json_transform(json(g.value->'processes')) AS p
+        ON p.index < json_array_length(g.value->'processes')
+) TO 'gpu_process_stats.parquet' (FORMAT PARQUET);
+
