c2c6ce277f528826d3890c83e4ef37bfb066c5a3
Dirk Petersen <no-reply@nowhere.com>
2025-07-04 12:10:18 -0700

    remove ceph stuff

--- ceph/ebs-delete-unused.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-for VOL in $(aws ec2 describe-volumes --filters "Name=status,Values=available" --query "Volumes[*].VolumeId" --output text); do     
-  echo "Deleting volume: $VOL"     
-  aws ec2 delete-volume --volume-id "$VOL"
-done
-
